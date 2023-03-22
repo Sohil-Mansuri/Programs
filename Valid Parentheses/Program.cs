2ebd03b3000d9143ea0f1c9749f8d1319ccfe62a
Sohil Mansuri <85024398+Sohil-Mansuri@users.noreply.github.com>
2023-03-22 19:58:17 +0530

    valid parenthesis code added
    first version added

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "({{})";
+            var result = Compare(input);
+        }
+
+        public static bool Compare(string input)
+        {
+            bool isMatch = true;
+            Stack<char> stackOfOpenParenthis = new Stack<char>();
+            for (int index = 0; index < input.Length; index++)
+            {
+                if(input[index] == '(' || input[index] == '[' || input[index] == '{')
+                {
+                    stackOfOpenParenthis.Push(input[index]);
+                }
+                else if(input[index] == ')')
+                {
+                    if(stackOfOpenParenthis.Peek() != '(')
+                    {
+                        isMatch = false;
+                        break;
+                    }
+                    else
+                        stackOfOpenParenthis.Pop();
+                }
+                else if (input[index] == '}')
+                {
+                    if (stackOfOpenParenthis.Peek() != '{')
+                    {
+                        isMatch = false;
+                        break;
+                    }
+                    else
+                        stackOfOpenParenthis.Pop();
+                }
+                else if (input[index] == ']')
+                {
+                    if (stackOfOpenParenthis.Peek() != '[')
+                    {
+                        isMatch = false;
+                        break;
+                    }
+                    else
+                        stackOfOpenParenthis.Pop();
+                }
+
+            }
+
+            if (stackOfOpenParenthis.Count > 0)
+                isMatch = false;
+
+            return isMatch;
+        }
+    }
+}
