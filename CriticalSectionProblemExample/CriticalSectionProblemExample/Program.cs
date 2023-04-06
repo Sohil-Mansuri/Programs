11cc92ddad873193efd6d9ce71abf522f43d9bc5
Sohil Mansuri <85024398+Sohil-Mansuri@users.noreply.github.com>
2023-04-06 11:12:33 +0530

    Critical problem section with reader lock

---
+++ CriticalSectionProblemExample/CriticalSectionProblemExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CriticalSectionProblemExample
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ReaderWriterLock myResultLock = new ReaderWriterLock();
+            List<string> testString1 = new List<string>
+            {
+                "Sohil","Mansuri","SM"
+            };
+
+            List<string> testString2 = new List<string>
+            {
+                "Sohil","Mansuri","SM"
+            };
+
+            Task.Factory.StartNew(() =>
+            {
+                foreach (var item in testString1)
+                {
+                    if (!myResultLock.IsReaderLockHeld)
+                    {
+                        myResultLock.AcquireReaderLock(Timeout.Infinite);
+                        Console.WriteLine($"from testring1 {item}");
+                        myResultLock.ReleaseReaderLock();
+                    }
+                }
+            });
+
+            Task.Factory.StartNew(() =>
+            {
+                foreach (var item in testString1)
+                {
+                    if (!myResultLock.IsReaderLockHeld)
+                    {
+                        myResultLock.AcquireReaderLock(Timeout.Infinite);
+                        Console.WriteLine($"from testring2 {item}");
+                        myResultLock.ReleaseReaderLock();
+                    }
+                }
+            });
+
+            Task.WaitAll();
+            Console.ReadLine();
+        }
+    }
+
+    
+}
